1af1bfed024530003d74b8bdb57e73436c6fbbff
Pvorret <phvorret@gmail.com>
2014-12-01 14:40:00 +0100

    Lavet en metod i controller der gør istand til man kan log in

--- Project3ProductionLtd/Controller.cs
+++ Project3ProductionLtd/Controller.cs
@@ -12,15 +12,44 @@ namespace Project3ProductionLtd
     class Controller
     {
         
-        public int logIn(string username, string password)
+        public int logIn(string inuserName, string inpassword)
         {
+            string userName = inuserName;
+            string password = inpassword;
+
+            SqlConnection connect = new SqlConnection(
+                "Server=ealdb1.eal.local;" +
+                "Database=EJL01_DB;" +
+                "User Id=ejl01_usr;" +
+                "Password=Baz1nga1"
+                );
+            try
+            {
+                connect.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
 
 
+            SqlCommand sqlCmd = new SqlCommand("getLoginId", connect);
+
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            sqlCmd.Parameters.Add(new SqlParameter("@UserName", userName));
+            sqlCmd.Parameters.Add(new SqlParameter("@Password", password));
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+
+            int id = int.Parse(reader["Id"].ToString());
+
+            return id;
 
         }
 
 
-        public void connectToSql()
+        public static void connectToSql()
         {
             SqlConnection connect = new SqlConnection(
                 "Server=ealdb1.eal.local;" +
