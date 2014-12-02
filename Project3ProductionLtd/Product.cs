d9204793bf5f22f9fb4f53b1a1c1ba4e4b6bfccf
Pvorret <phvorret@gmail.com>
2014-12-02 12:37:18 +0100

    Product(done)

--- Project3ProductionLtd/Product.cs
+++ Project3ProductionLtd/Product.cs
@@ -8,5 +8,22 @@ namespace Project3ProductionLtd
 {
     class Product
     {
+        public string Name { get; set; }
+        public DateTime OverallTime { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Spacing { get; set; }
+        public string Material { get; set; }
+        public double durability { get; set; }
+        public List<Machine> machineList = new List<Machine>();
+
+
+        public bool setProdMachineDeadline()
+        {
+
+
+            return true;
+        }
+
     }
 }
