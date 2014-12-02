a6457a2a62f7fd5609d8f0b1c8e824d41299c33e
thom2026 <thomas.b.n@hotmail.com>
2014-12-02 12:55:22 +0100

    Order

--- Project3ProductionLtd/Order.cs
+++ Project3ProductionLtd/Order.cs
@@ -8,5 +8,22 @@ namespace Project3ProductionLtd
 {
     class Order
     {
+        public Order(DateTime deadline, double width, double height, double spacing)
+        {
+            Deadline = deadline;
+            Width = width;
+            Height = height;
+            Spacing = spacing;
+        }
+        public Order(DateTime deadline, List<Product> products)
+        {
+            Deadline = deadline;
+            _products = products;
+        }
+        public DateTime Deadline { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Spacing { get; set; }
+        public List<Product> _products { get; set; }
     }
 }
