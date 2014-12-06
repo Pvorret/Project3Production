18bc8c371cc77edd655cebb118b3d1aa819ff12a
NicolajPedersen <nicolajgpedersen@gmail.com>
2014-12-06 13:41:56 +0100

    Produktionsarbejder class

---
+++ Project3ProductionLtd/Produktionsarbejder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3ProductionLtd {
+    class Produktionsarbejder {
+        public string Name { get; set; }
+        public List<Machine> machineList { get; set; }
+
+        public void Produktionsarbejder(string name) {
+            Name = name;
+        }
+    }
+}
