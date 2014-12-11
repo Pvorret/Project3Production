926b74e9f00f706feb085c0cccba8c0f44116315
Pvorret <phvorret@gmail.com>
2014-12-11 14:56:40 +0100

    Testing

---
+++ TestafOpretKunde/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project3ProductionLtd;
+
+namespace TestafOpretKunde
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            string expectedResult = "Customer Created";
+            string navn = "Peter";
+            string Adresse = "Bøgevej 34";
+            string Tlf = "25257777";
+            string Email = "peter@peter.dk";
+
+            //string actualResult = 
+
+        }
+    }
+}
